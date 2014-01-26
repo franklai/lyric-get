7237ee72bae5545f98ae5cbd6c20da161b078a1e
franklai <franklai@cmlab.csie.ntu.edu.tw>
2014-01-26 21:43:07 +0800

    add kasi_time

---
+++ lyric_engine/modules/kasi_time.py
@@ -0,0 +1,118 @@
+# coding: utf-8
+import os
+import sys
+include_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'include')
+sys.path.append(include_dir)
+
+import logging
+import common
+from lyric_base import LyricBase
+
+site_class = 'KasiTime'
+site_index = 'kasi_time'
+site_keyword = 'kasi-time'
+site_url = 'http://www.kasi-time.com/'
+test_url = 'http://www.kasi-time.com/item-262.html'
+test_expect_length = 1581
+
+# current url format
+# http://www.kasi-time.com/item-67546.html
+
+class KasiTime(LyricBase):
+    def parse_page(self):
+        url = self.url
+
+        if not self.find_lyric(url):
+            logging.info('Failed to get lyric of url [%s]', url)
+            return False
+
+        if not self.find_song_info(url):
+            logging.info('Failed to get song info of url [%s]', url)
+
+        return True
+
+    def find_lyric(self, url):
+        pattern = 'item-([0-9]+)\.html'
+
+        song_id = common.get_first_group_by_pattern(url, pattern)
+
+        if not song_id:
+            logging.info('Failed to get id of url [%s]', url)
+            return False
+
+        song_url = 'http://www.kasi-time.com/item_js.php?no=' + song_id
+        data = common.get_url_content(song_url)
+        if not data:
+            logging.info('Failed to get content of url [%s]', song_url)
+            return False
+
+        lyric = data.decode('utf-8', 'ignore')
+        lyric = lyric.replace("document.write('", "")
+        lyric = lyric.replace("');", "")
+        lyric = lyric.replace("<br>", "\n")
+        lyric = lyric.replace("&nbsp;", " ")
+        lyric = common.htmlspecialchars_decode(lyric)
+        lyric = common.strip_slash(lyric)
+        lyric = lyric.strip()
+
+        # test for half to full
+        lyric = common.half2full(lyric)
+
+        self.lyric = lyric
+
+        return True
+
+    def find_song_info(self, url):
+        ret = True
+        html = common.get_url_content(url)
+
+        encoding = 'utf-8'
+        html = html.decode(encoding, 'ignore')
+
+        pattern = '<h1>(.*)</h1>'
+        value = common.get_first_group_by_pattern(html, pattern).strip()
+        if value:
+            self.title = value
+        else:
+            logging.error('Failed to find title of url [%s]', url)
+            ret = False
+
+        prefix = '<div id="song_info_table">'
+        suffix = '</div>'
+        info_table = common.find_string_by_prefix_suffix(html, prefix, suffix)
+
+        patterns = {
+            'artist': u'歌手',
+            'lyricist': u'作詞',
+            'composer': u'作曲',
+            'arranger': u'編曲',
+        }
+
+        for key in patterns:
+            pattern = patterns[key]
+
+            prefix = u'<td class="td1">%s</td>' % (pattern)
+            suffix = '</a></td>'
+
+            value = common.find_string_by_prefix_suffix(info_table, prefix, suffix, False)
+            value = common.strip_tags(value).strip()
+            if value:
+                setattr(self, key, value)
+
+        return ret
+
+def get_lyric(url):
+    obj = KasiTime(url)
+
+    return obj.get()
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    url = 'http://www.kasi-time.com/item-67546.html'
+
+    full = get_lyric(url)
+    if not full:
+        print('Cannot get lyric')
+        exit()
+    print(full.encode('utf-8', 'ignore'))
---
+++ lyric_engine/tests/test_kasi_time.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+import os
+import sys
+module_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'modules')
+sys.path.append(module_dir)
+
+import unittest
+from kasi_time import KasiTime as Lyric
+
+class KasiTimeTest(unittest.TestCase):
+    def test_url_guren(self):
+        url = 'http://www.kasi-time.com/item-67546.html'
+        obj = Lyric(url)
+        obj.parse()
+
+        self.assertEqual(obj.title, u'紅蓮の弓矢')
+        self.assertEqual(obj.artist, u'Linked Horizon')
+        self.assertEqual(obj.lyricist, u'Revo')
+        self.assertEqual(obj.composer, u'Revo')
+        self.assertEqual(obj.arranger, u'Revo')
+        self.assertEqual(len(obj.lyric), 808)
+
+    def test_url_shiranai(self):
+        url = 'http://www.kasi-time.com/item-43855.html'
+        obj = Lyric(url)
+        obj.parse()
+
+        self.assertEqual(obj.title, u'君の知らない物語')
+        self.assertEqual(obj.artist, u'supercell')
+        self.assertEqual(obj.lyricist, u'ryo(supercell)')
+        self.assertEqual(obj.composer, u'ryo(supercell)')
+        self.assertEqual(obj.arranger, u'ryo(supercell)')
+        self.assertEqual(len(obj.lyric), 565)
+
+if __name__ == '__main__':
+    unittest.main()
