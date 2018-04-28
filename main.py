999333ecdc5c2d2ed1d78fc055f1c93d51e24481
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2018-04-29 01:34:14 +0800

    move lyric_get web to static folder
    and setup Google App Engine Flexible environment

--- main.py
+++ main.py
@@ -1,48 +1,58 @@
-#!/usr/bin/env python
 import json
+import logging
+
+import flask
+
 import lyric_engine
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
-    def handler(self):
-        url = self.request.get('url')
+app = flask.Flask(__name__, template_folder='static')
 
-        if url:
-            try:
-                engine = lyric_engine.Lyric(url)
-                lyric = engine.get_lyric()
-                output = json.dumps({'lyric': lyric})
-            except IOError:
-                output = json.dumps({'lyric': 'IO error!'})
-            except TypeError:
-                output = json.dumps({'lyric': 'Type error!'})
-            except IndexError:
-                output = json.dumps({'lyric': 'Index error!'})
-            except StandardError:
-                output = json.dumps({'lyric': 'Standard Error!'})
-        else:
-            output = json.dumps({'lyric': 'error!'})
+@app.route('/')
+def home():
+    """index page"""
+    return flask.render_template('index.html')
 
-        self.response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
-        self.response.headers.add('Access-Control-Allow-Methods', 'GET,POST')
+@app.route('/app')
+def main():
+    """Main lyric get handler"""
+    url = flask.request.args.get('url')
 
-        self.response.write(output)
-        
-    def get(self):
-        self.handler()
+    if url:
+        try:
+            engine = lyric_engine.Lyric(url)
+            lyric = engine.get_lyric()
+            output = json.dumps({'lyric': lyric})
+        except IOError:
+            output = json.dumps({'lyric': 'IO error!'})
+        except TypeError:
+            output = json.dumps({'lyric': 'Type error!'})
+        except IndexError:
+            output = json.dumps({'lyric': 'Index error!'})
+        except StandardError:
+            output = json.dumps({'lyric': 'Standard Error!'})
+    else:
+        output = json.dumps({'lyric': 'error!'})
 
-    def post(self):
-        self.handler()
+    response = flask.make_response(output)
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST'
+
+    return response
+
+    self.response.write(output)
 
 
-class NotFound(webapp2.RequestHandler):
-    def get(self):
-        self.redirect('http://franks543.blogspot.com/')
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
 
-    def post(self):
-        self.redirect('http://franks543.blogspot.com/')
 
-app = webapp2.WSGIApplication([
-    ('/app', MainHandler),
-    ('/.*', NotFound)
-], debug=True)
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=3000, debug=True)
+# [END app]
